29c2fdd368f45954672a1cb9bced3d4c93528960
Sadhurya30 <132685899+Sadhurya30@users.noreply.github.com>
2023-05-09 00:08:55 +0530

    Add files via upload

---
+++ day2/Student.java
@@ -0,0 +1,26 @@
+package week1.day2;
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Sadhurya";
+		System.out.println(name);
+		String gender = "Female" ;
+		System.out.println(gender);
+		String address = "No 20 1st street rajaji nagar salem -88" ;
+		System.out.println(address);
+		long phoneNumber = 9444068870L;
+		System.out.println(phoneNumber);
+		String emailId = "sadhupari07@gmail.com";
+		System.out.println(emailId);
+		short rollNumber = 5;
+		System.out.println(rollNumber);
+		String dateOfBirth = "30/08/1999";
+		System.out.println(dateOfBirth);
+		
+		
+
+	}
+
+}
