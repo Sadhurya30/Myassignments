2cc8121fe4aedf1e87a1b4c9194e8ff8e8687bbf
Sadhurya30 <132685899+Sadhurya30@users.noreply.github.com>
2023-05-11 21:19:08 +0530

    Add files via upload

---
+++ day44/Benz.java
@@ -0,0 +1,16 @@
+package week1.day44;
+
+import week1.day4.Car;
+
+public class Benz extends Car {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	Benz volkswagen = new Benz();
+    volkswagen.switchOnAc();
+
+    
+	}
+
+}
